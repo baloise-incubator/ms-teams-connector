01509d71169e147e155b454b520b7432d2e6e528
Arno Burkhart <arburk@users.noreply.github.com>
2023-04-07 14:54:44 +0200

    #2 added builder capability

--- src/main/java/com/baloise/open/ms/teams/templates/MessageCardFactory.java
+++ src/main/java/com/baloise/open/ms/teams/templates/MessageCardFactory.java
@@ -15,12 +15,29 @@
  */
 package com.baloise.open.ms.teams.templates;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Map;
 
 public final class MessageCardFactory {
 
-  private MessageCardFactory() {
-    // Do not instantiate
+  enum Color {
+    RED("#ff0000"),
+    GREEN("#1aff00"),
+    YELLOW("#ffee00"),
+    BLUE("#0600ab");
+
+    final String hexCode;
+
+    Color(String hexCode) {
+      this.hexCode = hexCode;
+    }
+  }
+
+  final MessageCard instance;
+
+  private MessageCardFactory(String title, String message) {
+    instance = createSimpleMessageCard(title, message);
   }
 
   public static MessageCard createSimpleMessageCard(final String title, final String message) {
@@ -35,4 +52,32 @@ public final class MessageCardFactory {
     return messageCard;
   }
 
+  public static MessageCardFactory builder(final String title, final String message) {
+    return new MessageCardFactory(title, message);
+  }
+
+  public MessageCardFactory withColor(final Color color) {
+    if (color != null) {
+      instance.setThemeColor(color.hexCode);
+    }
+    return this;
+  }
+
+  public MessageCardFactory withFact(final Fact fact) {
+    if (fact != null) {
+      instance.getSections().get(0).getFacts().add(fact);
+    }
+    return this;
+  }
+
+  public MessageCardFactory withFact(final String key, final String value) {
+    if(StringUtils.isNotBlank(key) && StringUtils.isNotBlank(value)) {
+      return withFact(new Fact(key, value));
+    }
+    return this;
+  }
+
+  public MessageCard build() {
+    return instance;
+  }
 }
--- src/test/java/com/baloise/open/ms/teams/templates/MessageCardFactoryTest.java
+++ src/test/java/com/baloise/open/ms/teams/templates/MessageCardFactoryTest.java
@@ -10,9 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MessageCardFactoryTest {
 
   private static final String SCHEMA = "http://schema.org/extensions";
-  private static final String COLOR = "0076D7";
   private static final String TYPE = "MessageCard";
-
 
   @Test
   void testSimpleMessageCreation() {
@@ -69,4 +67,53 @@ class MessageCardFactoryTest {
     );
   }
 
+  @Test
+  void testBuilder() {
+    final String title = "Whatever Title to use";
+    final String text = "And even some content to be included";
+    final String factKey = "fk1";
+    final String factValue = "fact value to be set";
+
+    final MessageCard messageCard = MessageCardFactory.builder(title, text)
+        .withColor(MessageCardFactory.Color.RED)
+        .withFact(factKey, factValue)
+        .withFact(null, "")
+        .withFact(" ", null)
+        .withFact(" A ", "B")
+        .build();
+
+    final Section section = messageCard.getSections().get(0);
+    final List<Fact> facts = section.getFacts();
+
+    assertAll(
+        () -> assertNull(messageCard.getTitle()),
+        () -> assertEquals(title, messageCard.getSummary()),
+        () -> assertEquals(SCHEMA, messageCard.getContext()),
+        () -> assertNotNull(messageCard.getThemeColor()),
+        () -> assertEquals(MessageCardFactory.Color.RED.hexCode, messageCard.getThemeColor()),
+        () -> assertEquals(TYPE, messageCard.getType()),
+        () -> assertNull(messageCard.getPotentialActions()),
+        () -> assertEquals(1, messageCard.getSections().size()),
+        () -> assertEquals(title, section.getActivityTitle()),
+        () -> assertEquals(text, section.getActivitySubtitle()),
+        () -> assertNull(section.getActivityImage()),
+        () -> assertNull(section.getMarkdown()),
+        () -> assertEquals(2, facts.size()),
+        () -> assertTrue(facts.contains(new Fact(factKey,factValue))),
+        () -> assertTrue(facts.contains(new Fact(" A ","B")))
+    );
+  }
+
+  @Test
+  void testColors() {
+    assertEquals(4, MessageCardFactory.Color.values().length);
+    assertEquals(MessageCardFactory.Color.RED, MessageCardFactory.Color.valueOf("RED"));
+    assertEquals(MessageCardFactory.Color.GREEN, MessageCardFactory.Color.valueOf("GREEN"));
+    assertEquals(MessageCardFactory.Color.BLUE, MessageCardFactory.Color.valueOf("BLUE"));
+    assertEquals(MessageCardFactory.Color.YELLOW, MessageCardFactory.Color.valueOf("YELLOW"));
+    assertEquals("#ff0000", MessageCardFactory.Color.RED.hexCode);
+    assertEquals("#1aff00", MessageCardFactory.Color.GREEN.hexCode);
+    assertEquals("#0600ab", MessageCardFactory.Color.BLUE.hexCode);
+    assertEquals("#ffee00", MessageCardFactory.Color.YELLOW.hexCode);
+  }
 }
