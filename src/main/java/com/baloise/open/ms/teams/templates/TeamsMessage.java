77fb2da9e5fe350cf46429b474703cf4b1be087b
B049309 <martin.bartolome@baloise.ch>
2025-11-19 12:17:55 +0100

    add TeamsMessage wrapper class for messages

---
+++ src/main/java/com/baloise/open/ms/teams/templates/TeamsMessage.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2025 Baloise Group
+ */
+package com.baloise.open.ms.teams.templates;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class TeamsMessage {
+    private final String type = "message";
+    private String summary;
+    @Builder.Default
+    @SerializedName("attachments")
+    private List<Attachment> attachments = new ArrayList<>();
+}
+
